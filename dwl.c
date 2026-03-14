eb5af6345ea842da6d517a72e5254e16d1af232f
Paul Chambaz <paul.chambaz@tutanota.com>
2026-03-14 23:53:57 +0100

    fix for tablet input

--- dwl.c
+++ dwl.c
@@ -3359,7 +3359,6 @@ tabletapplymap(double x, double y, struct wlr_input_device *dev)
 		}
 	}
 	wlr_cursor_map_input_to_region(cursor, dev, &geom);
-	wlr_cursor_map_input_to_output(cursor, dev, selmon->wlr_output);
 }
 
 void
